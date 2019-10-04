05ad09becb907e90621f3ca8e1fd4f9d51029825
FranElfers <39459727+FranElfers@users.noreply.github.com>
2019-10-04 17:30:38 -0300

    Update views.py

--- wans/views.py
+++ wans/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import PedidoForm, ClienteForm
+from django.contrib.auth.models import Group
 #from django.contrib.auth.models import User
 #from django.contrib.auth.decorators import login_required
 #from django.shortcuts import get_object_or_404
