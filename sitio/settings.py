caed6f63633796a539ca47a23ac68071129b3383
FranElfers <39459727+FranElfers@users.noreply.github.com>
2019-10-24 08:35:39 -0300

    Update settings.py

--- sitio/settings.py
+++ sitio/settings.py
@@ -115,4 +115,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
-STATIC_URL = (os.path.join(BASE_DIR, 'static'))
+STATIC_URL = '/static/'
